a7de26b4008652bfe10a07320182b5f87cff19fd
CatSci <atul.yadav@catsci.com>
2023-11-20 13:31:27 +0000

    line plot

--- eln_app.py
+++ eln_app.py
@@ -6,11 +6,10 @@ import tempfile, sys, io
 from PIL import Image
 
 # from src.saved_plots.test import create_stacked_bar_chart, create_heatmap, create_bar_plot, create_scatter_plot
-from src.utils.bar_chart import create_bar_plot
-from src.utils.stacked_bar_chart import create_stacked_bar_chart
+from src.utils.bar_plot import create_bar_chart
 from src.utils.heatmap import create_heatmap
 from src.utils.scatter_plot import create_scatter_plot
-from src.utils.test_plot import create_test_bar
+from src.utils.line_plot import create_line_plot
 
 from src.eln.eln import update_plot, update_plot_plotly
 import requests
@@ -54,7 +53,7 @@ if uploaded_file is not None:
     df.replace('-', 0, inplace= True)
 
     # Select plot type
-    plot_type = st.sidebar.selectbox("Select Plot Type", ["Heatmap", "Scatter Plot", "Bar Chart"])
+    plot_type = st.sidebar.selectbox("Select Plot Type", ["Heatmap", "Scatter Plot", "Line Plot","Bar Chart"])
 
     # Plot based on selected options
 
@@ -66,7 +65,7 @@ if uploaded_file is not None:
         y_columns = st.sidebar.multiselect('Select Y-axis Column', list(df.select_dtypes(include=['int64', 'float64']).columns))
         stacked_bar = st.sidebar.checkbox('Stacked Bar Chart')
         if x_columns and y_columns:
-            create_test_bar(df, x_columns, y_columns, stacked_bar)
+            create_bar_chart(df, x_columns, y_columns, stacked_bar)
         else:
             st.warning("Please select columns to plot data")
 
@@ -82,6 +81,9 @@ if uploaded_file is not None:
     #### Scatter Plot ####
     if plot_type == "Scatter Plot":
         create_scatter_plot(df)
+
+    if plot_type == "Line Plot":
+        create_line_plot(df= df)
 
 else:
     st.warning("Upload a file to get started.")
